9b3bcb165884dc0d2e905dc4fad691d871d7adf7
Victor Oliveira <victor.barbosa@hotelengine.com>
2024-10-13 20:49:21 -0300

    corrects division name on dashboard and adds click handler to trainer name to open history

--- src/templates/Dashboard/components/TrainerInfo/index.tsx
+++ src/templates/Dashboard/components/TrainerInfo/index.tsx
@@ -1,20 +1,31 @@
-import { useContext } from "solid-js";
+import { createResource, useContext } from "solid-js";
+
+import { League } from "@webfoot/core/models";
 
 import { ClubContext } from "../../contexts/club";
+import { LayoutContext } from "../../contexts/layout";
 
 export default function TrainerInfo() {
   const club = useContext(ClubContext);
+  const {
+    handlers: { setModalTrainerHistoryOpened },
+  } = useContext(LayoutContext);
+
+  const [league] = createResource(async () => League.getById(club().championship!.leagueId));
+
   return (
     <div class="flex pb-2 px-3 gap-1">
       <div class="w-12 flex items-end justify-end">
         <img src="/src/assets/flags/BRA.BMP" class="w-full" />
       </div>
       <div class="flex-1 flex flex-col justify-between">
-        <h2 class="font-bold text-sm">{club().trainer!.name}</h2>
+        <h2 class="font-bold text-sm" onClick={() => setModalTrainerHistoryOpened(true)}>
+          {club().trainer!.name}
+        </h2>
         <p class="mb-2 ml-2">Brasil</p>
       </div>
       <div class="flex-1 flex flex-col justify-end">
-        <p>1ª Divisão</p>
+        <p>{league()?.name}</p>
       </div>
     </div>
   );
--- src/templates/Dashboard/contexts/club.tsx
+++ src/templates/Dashboard/contexts/club.tsx
@@ -1,18 +1,20 @@
 import { type ParentProps, createContext, createResource } from "solid-js";
 import { useNavigate, useParams } from "@solidjs/router";
 
-import { GameLoop, Player, Team, Trainer } from "@webfoot/core/models";
-import type { IPlayer, ITeam, ITrainer } from "@webfoot/core/models/types";
+import { Championship, GameLoop, Player, Team, Trainer } from "@webfoot/core/models";
+import type { IChampionship, IPlayer, ITeam, ITrainer } from "@webfoot/core/models/types";
 import useDBReady from "@webfoot/hooks/useDBReady";
 import type { Context } from "@webfoot/utils/types";
 
 export type IClubContext = Context<{
+  championship: IChampionship;
   players: IPlayer[];
   team: ITeam;
   trainer: ITrainer;
 }>;
 
 export const ClubContext = createContext<IClubContext>(() => ({
+  championship: null,
   players: null,
   ready: false as const,
   team: null,
@@ -43,16 +45,21 @@ export default function DataProvider(props: ParentProps) {
   const [players] = createResource(trainer, async () =>
     Player.getMultipleByIndex("teamId", trainer()!.teamId!),
   );
+  const [championship] = createResource(team, async () =>
+    Championship.getById(team()!.championshipId!),
+  );
 
   const signal: IClubContext = () =>
-    players.state === "ready" && team.state === "ready"
+    players.state === "ready" && championship.state === "ready"
       ? {
+          championship: championship()!,
           players: players()!,
           ready: true,
           team: team()!,
           trainer: trainer()!,
         }
       : {
+          championship: null,
           players: null,
           ready: false,
           team: null,
